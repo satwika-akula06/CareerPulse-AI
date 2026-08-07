ccf143facf22f04f57df52c78cdda36eb2ae4ae3
satwika-akulao6 <satwikaakula6@gmail.com>
2026-08-07 17:02:03 +0530

    Initial commit

---
+++ src/routes/auth.routes.js
@@ -0,0 +1,38 @@
+const {Router} = require("express")
+const authController=require("../controllers/auth.controller")
+const authMiddleware=require("../middlewares/auth.middleware")
+
+const authRouter=Router()
+
+
+/**
+ * @route POST /api/auth/register
+ * @description Register a new user
+ * @access Public
+ */
+authRouter.post("/register", authController.registerUserController)
+
+
+/**
+ * @route POST /api/auth/login
+ * @description Login a user with email and password
+ * @access Public
+ */
+authRouter.post("/login", authController.loginUserController)
+ 
+
+/**
+ * @route POST /api/auth/logout
+ * @description clear token from user cookie and add it to the blacklist
+ * @access Public
+ */
+authRouter.get("/logout", authController.logoutUserController)
+
+/**
+ * @route GET /api/auth/get-me
+ * @description Get the current logged in user details
+ * @access Private
+ */
+authRouter.get("/get-me", authMiddleware.authUser, authController.getMeController)
+ 
+module.exports=authRouter
